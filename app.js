14a32f05179ae6df76f6cf219617a2c7eec16946
MyLittleViruzbx1 <mylittleviruz@outlook.es>
2022-10-06 14:19:20 -0600

    primera seccion terminada 1h 3min

---
+++ app.js
@@ -0,0 +1,24 @@
+
+// Variables con LET
+
+
+
+
+// variables en javascript 
+
+// var producto = "Monitor de 27 pulgadas";
+
+// las variables se pueden resignar
+// producto = 'Monitor de 21 pulgadas';
+
+
+// console.log('Hola')
+
+
+// const nombre = prompt('Cual es tu nombre');
+// console.log(nombre)
+// document.querySelector('.contenido').innerHTML = `${nombre} esta aprendiendo javascript moderno`
+// 
+
+
+// alert('hola')
