d247841cbb8f91ffbf6673466015f8db5a4dfe17
MyLittleViruzbx1 <mylittleviruz@outlook.es>
2022-10-08 02:49:38 -0600

    Cuarta seccion terminada 26min

---
+++ app2.js
@@ -0,0 +1,39 @@
+// NUMEROS
+
+const numero = new Number(72);
+console.log(numero)
+
+let resultado;
+
+resultado = Math.PI;
+console.log(resultado)
+resultado = Math.round(2.7);
+console.log(resultado)
+
+// redondea hacia arriba
+resultado = Math.ceil(2.1);
+console.log(resultado);
+
+// redondea hacia abajo
+resultado = Math.floor(2.9);
+console.log(resultado);
+
+// raiz
+resultado = Math.sqrt(144);
+console.log(resultado);
+// potencia 
+resultado= Math.pow(2,4);
+console.log(resultado);
+// minimo
+resultado = Math.min(3,5,7,3,23,-1);
+console.log(resultado)
+// maximo
+resultado = Math.max(1,2,4,5,6,7,8,10);
+console.log(resultado);
+// aleatorio
+resultado = Math.random();
+console.log(resultado)
+
+
+const numerito = "21";
+console.log(Number.parseInt(numerito));
